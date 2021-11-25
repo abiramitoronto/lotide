db0d13a43459055476569adedf7a4c24dbd18bb8
abiramitoronto <nagaabirami.toronto@gmail.com>
2021-11-25 16:33:57 -0500

    finding equal objects

--- eqObjects.js
+++ eqObjects.js
@@ -7,11 +7,30 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = function(arr1,arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
 const eqObjects = function(object1, object2) {
   let check = false;
   if (Object.keys(object1).length === Object.keys(object2).length) {
     for (const itr in object1) {
-      if (object1[itr] === object2[itr]) {
+      if (Array.isArray(object1[itr])) {
+        if (eqArrays(object1[itr],object2[itr])) {
+          check = true;
+        } else {
+          return false;
+        }
+      } else if (object1[itr] === object2[itr]) {
         check = true;
       } else {
         return false;
@@ -29,3 +48,10 @@ assertEqual(eqObjects(ab, ba),true); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
 assertEqual(eqObjects(ab, abc),false); // => false
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc),true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2),true); // => false
