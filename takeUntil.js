9c18cacd58063e2747ce757037ea477c9f06b763
abiramitoronto <nagaabirami.toronto@gmail.com>
2021-11-26 17:24:44 -0500

    Loop Thru Array Until Callback Func Condition

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,24 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`   Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`   Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1,arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
 const takeUntil = function(array, callback) {
   let temp = [];
   for (const val of array) {
@@ -20,3 +41,6 @@ console.log('---');
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+
+assertArraysEqual(takeUntil(data1, x => x < 0),[1, 2, 5, 7, 2]);
+assertArraysEqual(takeUntil(data2, x => x === ','),["I've", "been", "to", "Hollywood"]);
