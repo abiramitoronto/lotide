04d89b8c2e8eb42315f0581873ea692a49db7966
abiramitoronto <nagaabirami.toronto@gmail.com>
2021-11-29 21:28:42 -0500

    Mocha & Chai Test

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,20 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+//const assertEqual = require('../assertEqual');
+//const tail = require('../tail');
+//const result = tail(["Hello", "Lighthouse", "Labs"]);
+//assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
 
 // Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+//const words = ["Yo Yo", "Lighthouse", "Labs"];
+//tail(words); // no need to capture the return value since we are not checking it
+//assertEqual(words.length, 3); // original array should still have 3 elements!
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.strictEqual(tail([1, 2, 3]), [2, 3]);
+  });
+},it("returns ['5'] for ['5']", () => {
+  assert.strictEqual(tail(['5']), ['5']); 
+}));
