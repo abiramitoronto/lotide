6f3ed08ee7c11c3bdf1b383aeed4c71021df46e4
abiramitoronto <nagaabirami.toronto@gmail.com>
2021-11-29 21:36:36 -0500

    Mocha & Chai for tail and middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,16 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const eqArrays = require('../eqArrays');
-const middle = require('../middle');
-assertArraysEqual(eqArrays(middle([1, 2, 3]), [2]),true); // => should PASS
-assertArraysEqual(eqArrays(middle([1, 2, 3, 4]), [2, 3]),true); // => should PASS
+//const assertArraysEqual = require('../assertArraysEqual');
+//const eqArrays = require('../eqArrays');
+//const middle = require('../middle');
+//assertArraysEqual(eqArrays(middle([1, 2, 3]), [2]),true); // => should PASS
+//assertArraysEqual(eqArrays(middle([1, 2, 3, 4]), [2, 3]),true); // => should PASS
+
+const assert = require('chai').assert;
+const middle   = require('../middle');
+
+describe("#tail", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+},it("returns [2, 3] for [1, 2, 3, 4]", () => {
+  assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]); 
+}));
